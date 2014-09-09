8ee52356ca63611556782bbf860adc8e7f717ff2
Isael Sousa <faelp22@hotmail.com>
2014-09-09 16:38:14 -0300

    Organizando doc

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Commom_hash V-0.0.1</title>
     </head>
     <body>
         <?php
-            require_once './vendor/autoload.php';
+            if(file_exists('./vendor/autoload.php')):
+                require_once './vendor/autoload.php';
+            else:
+                require_once dirname(dirname(__DIR__)).'/autoload.php';
+            endif;
+            
             echo \common_hash\Libs\Bcrypt::hash('123456');
             echo '<br />';
             echo \common_hash\Libs\GenIdHash::genIdHash();
